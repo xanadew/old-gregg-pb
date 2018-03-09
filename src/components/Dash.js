99b16348639b5a647ad93e6a139e4c36151054b1
Greg Marvin <gregmarv@gmail.com>
2018-03-09 11:30:44 -0700

    dash done

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -12,12 +12,14 @@ class Dash extends Component {
         return (
             <div className="Dash">
                 <nav className="nav">
-                <div className="logout"></div>
+                
                     <div onClick={()=>this.setState({slide:!this.state.slide})}
-                        className="arrow"></div>
+                        className="arrow">V</div>
+                        <a href="http://localhost:3000/">
+                        <button className="logout">BEGONE</button></a>
                 </nav>
                 <div className={this.state.slide?'slide dropdown':'dropdown'}>
-                    <div className='fs_locator'>Field/Store Locator</div>
+                    <button className='fs_locator'>Field/Store Locator</button>
                     <div className='rec_stores'>Recommended Online Stores</div>
                     <div className='fs_reviews'>Field/Store Reviews</div>
                     <div className='forums'>Forums</div>
